746768b7cf58209ba7b1654b4f1bfc9aa6cda141
Alex José Silva <turmawb@gmail.com>
2024-06-21 09:01:14 -0300

    Update orderService.js

--- services/orderService.js
+++ services/orderService.js
@@ -3,21 +3,32 @@ const fileHandler = require('../utils/fileHandler');
 const orders = [];
 
 exports.processFile = async (filePath) => {
-    const data = fs.readFileSync(filePath, 'utf8');
-    const lines = data.split('\n');
-    const processedOrders = fileHandler.processLines(lines);
-    orders.push(...processedOrders);
-    return processedOrders;
+    try {
+        const data = fs.readFileSync(filePath, 'utf8');
+        const lines = data.split('\n');
+        const processedOrders = fileHandler.processLines(lines);
+        orders.push(...processedOrders);
+        return processedOrders;
+    } catch (error) {
+        console.error('Erro ao processar o arquivo:', error);
+        throw error; // ou retorne um valor de erro apropriado
+    }
 };
 
 exports.getOrders = ({ startDate, endDate }) => {
     let filteredOrders = orders;
     if (startDate && endDate) {
-        filteredOrders = orders.filter(order => order.date >= startDate && order.date <= endDate);
+        filteredOrders = orders.filter(order => {
+            const orderDate = new Date(order.date);
+            return orderDate >= startDate && orderDate <= endDate;
+        });
     }
     return filteredOrders;
 };
 
 exports.getOrderById = (orderId) => {
+    if (typeof orderId !== 'number') {
+        throw new Error('orderId deve ser um número.');
+    }
     return orders.find(order => order.order_id === parseInt(orderId, 10));
 };
